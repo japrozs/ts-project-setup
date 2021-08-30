b9e9b49bd263c4ea4ecdcbfe3c73bb76d6a65e16
Japroz-Saini <sainijaproz@gmail.com>
2021-08-30 09:41:23 +0530

    :bulb: initial commit

---
+++ packages/server/dist/index.js
@@ -0,0 +1,72 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const constants_1 = require("./constants");
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const user_1 = require("./resolvers/user");
+const ioredis_1 = __importDefault(require("ioredis"));
+const express_session_1 = __importDefault(require("express-session"));
+const connect_redis_1 = __importDefault(require("connect-redis"));
+const cors_1 = __importDefault(require("cors"));
+const typeorm_1 = require("typeorm");
+const User_1 = require("./entities/User");
+const path_1 = __importDefault(require("path"));
+const main = async () => {
+    const conn = await (0, typeorm_1.createConnection)({
+        type: "postgres",
+        database: "axial",
+        username: "postgres",
+        password: "postgres",
+        logging: true,
+        migrations: [path_1.default.join(__dirname, "./migrations/*")],
+        synchronize: true,
+        entities: [User_1.User],
+    });
+    conn.runMigrations();
+    const app = (0, express_1.default)();
+    const RedisStore = (0, connect_redis_1.default)(express_session_1.default);
+    const redis = new ioredis_1.default();
+    app.use((0, cors_1.default)({
+        origin: "http://localhost:19006",
+        credentials: true,
+    }));
+    app.use((0, express_session_1.default)({
+        name: constants_1.COOKIE_NAME,
+        store: new RedisStore({
+            client: redis,
+            disableTouch: true,
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+            httpOnly: true,
+            sameSite: "lax",
+            secure: constants_1.__prod__,
+        },
+        saveUninitialized: false,
+        secret: "4BCB54273A146FF762EF27C2F32A630CC558AB6043B453C8461A9BD5E2BEB35B",
+        resave: false,
+    }));
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({
+            resolvers: [user_1.UserResolver],
+            validate: false,
+        }),
+        context: ({ req, res }) => ({ req, res, redis }),
+    });
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
+    app.listen(4000, () => {
+        console.log("🚀 Server started on localhost:4000");
+    });
+};
+main().catch((err) => {
+    console.error(err);
+});
+//# sourceMappingURL=index.js.map
---
+++ packages/server/src/index.ts
@@ -0,0 +1,78 @@
+import "reflect-metadata";
+import { COOKIE_NAME, __prod__ } from "./constants";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/user";
+import Redis from "ioredis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
+import { createConnection } from "typeorm";
+import { User } from "./entities/User";
+import path from "path";
+
+// rerun
+const main = async () => {
+    const conn = await createConnection({
+        type: "postgres",
+        database: "axial",
+        username: "postgres",
+        password: "postgres",
+        logging: true,
+        migrations: [path.join(__dirname, "./migrations/*")],
+        synchronize: true, // set to false, when wiping the data (i.e. await Post.delete({}); )
+        entities: [User],
+    });
+    conn.runMigrations();
+    const app = express();
+
+    const RedisStore = connectRedis(session);
+    const redis = new Redis();
+
+    app.use(
+        cors({
+            origin: "http://localhost:19006",
+            credentials: true,
+        })
+    );
+    app.use(
+        session({
+            name: COOKIE_NAME,
+            store: new RedisStore({
+                client: redis,
+                disableTouch: true,
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+                httpOnly: true,
+                sameSite: "lax",
+                secure: __prod__, // cookie only works in https (turn this off if not using https in production)
+            },
+            saveUninitialized: false,
+            secret: "4BCB54273A146FF762EF27C2F32A630CC558AB6043B453C8461A9BD5E2BEB35B",
+            resave: false,
+        })
+    );
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [UserResolver],
+            validate: false,
+        }),
+        context: ({ req, res }) => ({ req, res, redis }),
+    });
+
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
+
+    app.listen(4000, () => {
+        console.log("🚀 Server started on localhost:4000");
+    });
+};
+
+main().catch((err) => {
+    console.error(err);
+});
